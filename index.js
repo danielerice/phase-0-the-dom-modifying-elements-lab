d8fe96c1554678d3e6bfffb6a81b26afecf87a11
Daniel Rice <danielrice5627@gmail.com>
2022-11-18 11:41:48 -0800

    commit tree 7ca9372d5adb15ebf7a3ad3bc87b0a9aac424b75

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let mainMain = document.getElementById(`main`);
+mainMain.remove();
+let newHeader = document.createElement(`h1`);
+newHeader.setAttribute(`id`, `victory`);
+newHeader.textContent=`Daniel is the champion`;
